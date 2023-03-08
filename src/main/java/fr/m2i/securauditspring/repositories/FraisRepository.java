2ee205adae30c8cfa7d0dfce97fe481f59dff12c
Ibtissam Mdarbi <114956382+ibtmdr@users.noreply.github.com>
2023-03-08 15:46:47 +0100

    Delete src/main/java/fr/m2i/securauditspring/repositories directory

--- src/main/java/fr/m2i/securauditspring/repositories/FraisRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.m2i.securauditspring.repositories;
-
-import fr.m2i.securauditspring.model.Frais;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FraisRepository extends JpaRepository<Frais, Integer> {
-}
