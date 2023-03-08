5eb8d22d66d63aec4a7bc4a1aeccdd945ba6ff87
iTaghoury <idriss.taghoury@gmail.com>
2023-03-08 15:08:03 +0100

    Initial commit

---
+++ src/main/java/fr/m2i/securauditspring/SecurauditSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.m2i.securauditspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurauditSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurauditSpringApplication.class, args);
+    }
+
+}
