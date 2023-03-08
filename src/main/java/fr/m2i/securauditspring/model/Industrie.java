46ba42a0edddcca8c31c4ac843a1f9ed169de995
ycronel <caroline-lenorcy@orange.fr>
2023-03-08 15:42:50 +0100

    Created Industrie class file

---
+++ src/main/java/fr/m2i/securauditspring/model/Industrie.java
@@ -0,0 +1,52 @@
+package fr.m2i.securauditspring.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "industrie")
+public class Industrie {
+
+    @Id
+    @GeneratedValue
+    private int idIndustrie;
+    @Column(name = "siret")
+    private String siret;
+    @Column(name = "raisonSociale")
+    private String raisonSociale;
+
+
+    //region CONSTRUCTORS
+    public Industrie(){}
+    public Industrie(String siret, String raisonSociale) {
+        this.siret = siret;
+        this.raisonSociale = raisonSociale;
+    }
+    public Industrie(int idIndustrie, String siret, String raisonSociale) {
+        this.idIndustrie = idIndustrie;
+        this.siret = siret;
+        this.raisonSociale = raisonSociale;
+    }
+    //endregion
+
+    //region GETTERS AND SETTERS
+    public int getIdIndustrie() {
+        return idIndustrie;
+    }
+    public void setIdIndustrie(int idIndustrie) {
+        this.idIndustrie = idIndustrie;
+    }
+    public String getSiret() {
+        return siret;
+    }
+    public void setSiret(String siret) {
+        this.siret = siret;
+    }
+    public String getRaisonSociale() {
+        return raisonSociale;
+    }
+    public void setRaisonSociale(String raisonSociale) {
+        this.raisonSociale = raisonSociale;
+    }
+    //endregion
+
+}
