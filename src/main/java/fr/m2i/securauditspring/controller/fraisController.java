827d2fa350ee031e5b5fa096729e6dc70f6a284a
HP <ibtissammdarbi@gmail.com>
2023-03-09 10:00:01 +0100

    implementer select by id from frais

--- src/main/java/fr/m2i/securauditspring/controller/fraisController.java
+++ src/main/java/fr/m2i/securauditspring/controller/fraisController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/frais")
@@ -25,6 +27,17 @@ public class fraisController {
             return new ResponseEntity<List<Frais>>(fraisRepository.findAll(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/select/{id}")
+    public ResponseEntity<Frais> getFraisById(@PathVariable("id") Integer id) {
+        Optional<Frais> FraisData = fraisRepository.findById(id);
+
+        if (FraisData.isPresent()) {
+            return new ResponseEntity<>(FraisData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
